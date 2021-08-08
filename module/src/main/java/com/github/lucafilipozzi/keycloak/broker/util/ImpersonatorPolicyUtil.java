52185a0e80792a70db1c9cb7f1f375783066a592
Luca Filipozzi <luca.filipozzi@gmail.com>
2021-08-08 15:22:59 -0700

    remove redundant code

--- module/src/main/java/com/github/lucafilipozzi/keycloak/broker/util/ImpersonatorPolicyUtil.java
+++ module/src/main/java/com/github/lucafilipozzi/keycloak/broker/util/ImpersonatorPolicyUtil.java
@@ -6,13 +6,9 @@
 
 package com.github.lucafilipozzi.keycloak.broker.util;
 
-import com.google.common.collect.Sets;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.jboss.logging.Logger;
-import org.keycloak.models.ClientModel;
 import org.keycloak.models.RealmModel;
-import org.keycloak.models.RoleModel;
 import org.keycloak.models.UserModel;
 
 /**
@@ -44,27 +40,5 @@ public final class ImpersonatorPolicyUtil {
         }
       )
     );
-  }
-
-  public static void processUserRoleAssignmentsOld(RealmModel realm, UserModel user, Set<String> assertedGroupMemberships, String clientRoleAttributeName) {
-    realm.getClientsStream().forEach(client ->
-        client.getRolesStream()
-          .filter(clientRole -> clientRole.getName().endsWith(CLIENT_ROLE_SUFFIX))
-          .forEach(clientRole -> processUserRoleAssignments(client, clientRole, user, assertedGroupMemberships, clientRoleAttributeName))
-    );
-  }
-
-  private static void processUserRoleAssignments(ClientModel client, RoleModel clientRole, UserModel user, Set<String> assertedGroupMemberships, String clientRoleAttributeName) {
-    LOG.info("process user role assignments (private)");
-    LOG.info(clientRoleAttributeName);
-    Set<String> requiredGroupMemberships = clientRole.getAttributeStream(clientRoleAttributeName).collect(Collectors.toSet());
-    LOG.info(requiredGroupMemberships);
-    if (Sets.intersection(assertedGroupMemberships, requiredGroupMemberships).isEmpty()) {
-      LOG.infof("delete mapping client=%s role=%s user=%s", client.getClientId(), clientRole.getName(), user.getUsername());
-      user.deleteRoleMapping(clientRole);
-    } else {
-      LOG.infof("insert mapping client=%s role=%s user=%s", client.getClientId(), clientRole.getName(), user.getUsername());
-      user.grantRole(clientRole);
-    }
   }
 }
